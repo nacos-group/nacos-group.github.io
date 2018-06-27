aaf1523a409949058b1916d06e3255c990365cfa
何煦 <hexu.hxy@alibaba-inc.com>
2018-06-27 20:45:18 +0800

    增加 GA 统计

--- src/components/language/index.jsx
+++ src/components/language/index.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { autobind } from 'core-decorators';
 import cookie from 'js-cookie';
 import qs from 'querystring';
+import ReactGA from 'react-ga';
 
 export default class Language extends React.Component {
+  constructor() {
+    super();
+      ReactGA.initialize('UA-121449408-1');
+      // ReactGA.initialize('UA-121449408-1', { debug: true });
+  }
+
+  componentWillReceiveProps() {
+    const path = window.location.pathname + window.location.hash;
+    if (path.includes('/#/docs/')) {
+      if (path.includes('?lang=')) {
+        ReactGA.pageview(path);
+      }
+    } else {
+      ReactGA.pageview(path);
+    }
+  }
+
   @autobind
   onLanguageChange(language) {
     const { location } = this.props;
