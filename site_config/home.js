8414d43294bf23b324bd639e2d1a279c6309d8e3
xuechaos <xchaos8@126.com>
2018-12-07 18:13:52 +0800

    nacos version update 0.6.0 release

--- site_config/home.js
+++ site_config/home.js
@@ -15,14 +15,14 @@ export default {
         },
       ],
       version: {
-        text: 'V0.5.0',
+        text: 'V0.6.0',
         link: 'https://github.com/alibaba/nacos/releases',
       },
       note: {
         text: 'Release Note',
         link: 'https://github.com/alibaba/nacos/releases',
       },
-      releaseDate: 'Released on Nov 17, 2018',
+      releaseDate: 'Released on Dec 7, 2018',
     },
     functions: {
       title: 'Features',
@@ -102,14 +102,14 @@ export default {
         },
       ],
       version: {
-        text: 'V0.5.0',
+        text: 'V0.6.0',
         link: 'https://github.com/alibaba/nacos/releases',
       },
       note: {
         text: '版本说明',
         link: 'https://github.com/alibaba/nacos/releases',
       },
-      releaseDate: '2018年11月17日发布',
+      releaseDate: '2018年12月07日发布',
     },
     functions: {
       title: '功能',
