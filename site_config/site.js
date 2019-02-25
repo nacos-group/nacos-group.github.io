37e5f13231f7412f20bb64730c445bb3ff57c8a0
hxy1991 <cannel_2020@163.com>
2019-02-25 14:48:24 +0800

    Add AHAS

--- site_config/site.js
+++ site_config/site.js
@@ -65,6 +65,10 @@ export default {
               text: 'Cloud Service EDAS',
               link: 'https://www.aliyun.com/product/edas?source_type=nacos_pc_20181219',
           },
+          {
+              text: 'Cloud Service AHAS',
+              link: 'https://www.aliyun.com/product/ahas?source_type=nacos_pc_20190225',
+          },
       ],
     },
 
@@ -132,6 +136,10 @@ export default {
           {
               text: '云服务 EDAS',
               link: 'https://www.aliyun.com/product/edas?source_type=nacos_pc_20181219',
+          },
+          {
+              text: '云服务 AHAS',
+              link: 'https://www.aliyun.com/product/ahas?source_type=nacos_pc_20190225',
           },
       ],
     },
