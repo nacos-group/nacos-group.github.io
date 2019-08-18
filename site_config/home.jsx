6391087a600f2fd2cad1fe3b4528e29c4f004892
caojiele <caojiele1225@126.com>
2019-08-18 12:18:42 +0800

    Update Nacos release version 1.1.3 information

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -17,10 +17,10 @@ export default {
         },
       ],
       versionNote: {
-        text: 'Release Note of V1.1.0',
-        link: 'https://github.com/alibaba/nacos/releases/tag/1.1.0',
+        text: 'Release Note of V1.1.3',
+        link: 'https://github.com/alibaba/nacos/releases/tag/1.1.3',
       },
-      releaseDate: 'Released on Jul 06, 2019',
+      releaseDate: 'Released on Aug 06, 2019',
     },
     functions: {
       title: 'Features',
@@ -122,10 +122,10 @@ export default {
         },
       ],
       versionNote: {
-            text: 'V1.1.0 版本说明',
-            link: 'https://github.com/alibaba/nacos/releases/tag/1.1.0',
+            text: 'V1.1.3 版本说明',
+            link: 'https://github.com/alibaba/nacos/releases/tag/1.1.3',
         },
-      releaseDate: '2019年07月06日发布',
+      releaseDate: '2019年08月06日发布',
     },
     functions: {
       title: '功能',
