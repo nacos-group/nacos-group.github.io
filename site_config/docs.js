6f61301037907f15edf3f723a8ecd65b8fbff21f
KomachiSion <263976490@qq.com>
2020-12-04 18:36:15 +0800

    Add 2.0.0-ALPHA document

--- site_config/docs.js
+++ site_config/docs.js
@@ -136,6 +136,10 @@ export default {
             ],
           },
           {
+            title: 'Nacos 2.0',
+            link: '/en-us/docs/2.0.0-alpha.html'
+          },
+          {
             title: 'Contributor Guide',
             children: [
               {
@@ -310,6 +314,10 @@ export default {
                   link: '/zh-cn/docs/nacos-sync-use.html',
               },
             ],
+          },
+          {
+            title: 'Nacos 2.0',
+            link: '/zh-cn/docs/2.0.0-alpha.html'
           },
           {
             title: '开源共建',
