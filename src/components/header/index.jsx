0fdec6d12a8c208aa96b996a8fe79e438d20c363
杨翊 SionYang <xiweng.yy@alibaba-inc.com>
2021-08-06 10:33:40 +0800

    Revert "更新官网导航 (#214)"
    This reverts commit 96efdf64afe36bf9b1d9991a086d09fda30c7ddf.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -16,16 +16,16 @@ const languageSwitch = [
   },
 ];
 const searchSwitch = {
-  baidu: {
-    logo: 'https://img.alicdn.com/tfs/TB1n6DQayLaK1RjSZFxXXamPFXa-300-300.png',
-    url: 'https://www.baidu.com/s?wd=',
-  },
-  google: {
-    logo: 'https://img.alicdn.com/tfs/TB1REfuaCzqK1RjSZFjXXblCFXa-300-300.jpg',
-    url: 'https://www.google.com/search?q=',
-  },
+    baidu: {
+        logo: 'https://img.alicdn.com/tfs/TB1n6DQayLaK1RjSZFxXXamPFXa-300-300.png',
+        url: 'https://www.baidu.com/s?wd=',
+    },
+    google: {
+        logo: 'https://img.alicdn.com/tfs/TB1REfuaCzqK1RjSZFjXXblCFXa-300-300.jpg',
+        url: 'https://www.google.com/search?q=',
+    },
 };
-const noop = () => { };
+const noop = () => {};
 
 const defaultProps = {
   type: 'primary',
@@ -65,40 +65,40 @@ class Header extends React.Component {
     this.props.onLanguageChange(language);
   }
 
-  switchSearch() {
-    let search;
-    if (this.state.search === 'baidu') {
-      search = 'google';
-    } else {
-      search = 'baidu';
+    switchSearch() {
+        let search;
+        if (this.state.search === 'baidu') {
+            search = 'google';
+        } else {
+            search = 'baidu';
+        }
+        this.setState({
+            search,
+        });
     }
-    this.setState({
-      search,
-    });
-  }
 
-  toggleSearch() {
-    this.setState({
-      searchVisible: !this.state.searchVisible,
-    });
-  }
-
-  onInputChange(e) {
-    this.setState({
-      searchValue: e.target.value,
-    });
-  }
-
-  goSearch() {
-    const { search, searchValue } = this.state;
-    window.open(`${searchSwitch[search].url}${window.encodeURIComponent(`${searchValue} site:${siteConfig.domain}`)}`);
-  }
-
-  onKeyDown(e) {
-    if (e.keyCode === 13) {
-      this.goSearch();
+    toggleSearch() {
+        this.setState({
+            searchVisible: !this.state.searchVisible,
+        });
     }
-  }
+
+    onInputChange(e) {
+        this.setState({
+            searchValue: e.target.value,
+        });
+    }
+
+    goSearch() {
+        const { search, searchValue } = this.state;
+        window.open(`${searchSwitch[search].url}${window.encodeURIComponent(`${searchValue} site:${siteConfig.domain}`)}`);
+    }
+
+    onKeyDown(e) {
+        if (e.keyCode === 13) {
+            this.goSearch();
+        }
+    }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
@@ -122,43 +122,43 @@ class Header extends React.Component {
           <a href={getLink(`/${language}/index.html`)}>
             <img className="logo" alt={siteConfig.name} title={siteConfig.name} src={logo} />
           </a>
-          {
-            siteConfig.defaultSearch ?
-              (
-                <div
-                  className={classnames({
-                    search: true,
-                    [`search-${type}`]: true,
-                  })}
-                >
-                  <span className="icon-search" onClick={this.toggleSearch} />
-                  {
-                    searchVisible ?
-                      (
-                        <div className="search-input">
-                          <img src={searchSwitch[search].logo} onClick={this.switchSearch} />
-                          <input autoFocus onChange={this.onInputChange} onKeyDown={this.onKeyDown} />
+            {
+                siteConfig.defaultSearch ?
+                    (
+                        <div
+                            className={classnames({
+                                search: true,
+                                [`search-${type}`]: true,
+                            })}
+                        >
+                            <span className="icon-search" onClick={this.toggleSearch} />
+                            {
+                                searchVisible ?
+                                    (
+                                        <div className="search-input">
+                                            <img src={searchSwitch[search].logo} onClick={this.switchSearch} />
+                                            <input autoFocus onChange={this.onInputChange} onKeyDown={this.onKeyDown} />
+                                        </div>
+                                    ) : null
+                            }
                         </div>
-                      ) : null
-                  }
-                </div>
-              ) : null
-          }
+                    ) : null
+            }
           {
             onLanguageChange !== noop ?
-              (<span
-                className={
-                  classnames({
-                    'language-switch': true,
-                    [`language-switch-${type}`]: true,
-                  })
-                }
-                onClick={this.switchLang}
-              >
-                {languageSwitch.find(lang => lang.value === language).text}
-              </span>)
-              :
-              null
+            (<span
+              className={
+                classnames({
+                  'language-switch': true,
+                  [`language-switch-${type}`]: true,
+                })
+              }
+              onClick={this.switchLang}
+            >
+              {languageSwitch.find(lang => lang.value === language).text}
+            </span>)
+            :
+            null
           }
           <div
             className={
@@ -175,17 +175,16 @@ class Header extends React.Component {
             />
             <ul>
               {siteConfig[language].pageMenu.map((item) => (
-                <li
-                  key={item.key}
-                  className={classnames({
-                    'menu-item': true,
-                    [`menu-item-${type}`]: true,
-                    [`menu-item-${type}-active`]: currentKey === item.key,
-                  })}
-                >
-                  {item.link ? <span><a href={getLink(item.link)}>{item.text}</a>{item.imgUrl ? <img className={"menu-img"} src={item.imgUrl} /> : null}</span> : null}
-                  {item.children ? <div className="nav-container"><div className="word"><a >{item.text}</a>{item.imgUrl ? <img className={"menu-img"} src={item.imgUrl} /> : null}</div><ul className="sub-nav-container" style={{ width: language === 'zh-cn' ? 180 : 260 }}>{item.children.map((child) => <li><a href={child.link} target="_blank">{child.text}</a></li>)}</ul> </div> : null}
-                </li>
+                  <li
+                    key={item.link}
+                    className={classnames({
+                      'menu-item': true,
+                      [`menu-item-${type}`]: true,
+                      [`menu-item-${type}-active`]: currentKey === item.key,
+                    })}
+                  >
+                    <a href={getLink(item.link)}>{item.text}</a>{item.imgUrl ? <img className={"menu-img"} src={item.imgUrl} /> : null}
+                  </li>
               ))}
             </ul>
           </div>
