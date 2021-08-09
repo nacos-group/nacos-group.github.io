8d14f14100f32abbbaec97b1563d3d43916eef69
realJackSun <sjtusl@163.com>
2021-08-09 19:22:49 +0800

    Change default page to Chinese

--- site_config/site.js
+++ site_config/site.js
@@ -3,7 +3,7 @@ export default {
   port: 8080, // 本地开发服务器的启动端口
   domain: 'nacos.io', // 站点部署域名
   defaultSearch: 'baidu', // 默认搜索引擎，baidu或者google
-  defaultLanguage: 'en-us',
+  defaultLanguage: 'zh-cn',
   'en-us': {
     pageMenu: [
       {
