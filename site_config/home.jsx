47939178b2bfa08adbbf6c433967e59c72155aad
KomachiSion <263976490@qq.com>
2022-08-11 10:10:17 +0800

    更换公司logo

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -210,6 +210,8 @@ export default {
             list: [
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1561540563438-a13d7097-fcfd-400f-b066-82564ff31647.jpeg',
                 'https://img.alicdn.com/imgextra/i1/O1CN01GqMYP21Xjj7xttIj0_!!6000000002960-0-tps-700-383.jpg',
+                'https://cdn.nlark.com/yuque/0/2022/png/1577777/1660182838738-ab94039e-9c22-413c-9715-db9add1eb83c.png',
+                'https://cdn.nlark.com/yuque/0/2022/jpeg/1577777/1660182954580-7322d9df-c461-4991-ab95-fcdec773bd30.jpeg',
                 'https://img.alicdn.com/tfs/TB1aB62OBr0gK0jSZFnXXbRRXXa-280-160.jpg',
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1562465794060-b0a5d9eb-bb52-47cd-a3fd-1b30d805e859.jpeg',
                 'https://user-images.githubusercontent.com/975639/103077254-6dc24e80-460a-11eb-9ff4-81e42dff60c9.png',
@@ -224,11 +226,9 @@ export default {
                 'https://img.alicdn.com/tfs/TB1qhDSOrr1gK0jSZFDXXb9yVXa-280-160.png',
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1561540563550-c02c3ffc-75fc-41ae-898c-20b5e60d0e64.jpeg',
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1561540563512-e640635e-a180-4c4d-b004-6ed91863782d.jpeg',
-                'https://img.alicdn.com/tfs/TB1uPvQOpT7gK0jSZFpXXaTkpXa-280-160.png',
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1561540563467-2fc7ba94-f003-465e-ab62-df52aeb457d7.jpeg',
                 'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1569745984623-8f755a9b-f42d-49d2-88a7-cb58b1833a23.jpeg',
-                'https://img.alicdn.com/imgextra/i1/O1CN01CTgp0F1WFfW6nv8aV_!!6000000002759-2-tps-1660-712.png',
-                'https://cdn.nlark.com/yuque/0/2019/jpeg/338441/1561540563559-40f4bf4a-bc4c-45cf-9cf9-09280a34f0cc.jpeg'
+                'https://img.alicdn.com/imgextra/i1/O1CN01CTgp0F1WFfW6nv8aV_!!6000000002759-2-tps-1660-712.png'
             ],
         },
     },
