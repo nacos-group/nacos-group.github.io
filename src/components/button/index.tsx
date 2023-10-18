d15e578813decfe5c80ac2022a6354e99c9d3263
jimin <slievrly@163.com>
2023-10-18 11:49:08 +0800

    feature: add nacosCloud

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import classnames from 'classnames';
-import { getLink } from '../../utils';
-import './index.scss';
+import React from "react";
+import classnames from "classnames";
+import { getLink } from "../../utils";
+import "./index.scss";
 
-export type ButtonType = 'primary' | 'normal';
+export type ButtonType = "primary" | "normal";
 type Props = {
   type: string;
   link: string;
   target: string;
   children?: React.ReactNode;
   language?: string;
+  customStyle?: React.CSSProperties;
 };
 
 const defaultProps: Props = {
-  type: 'primary',
-  link: '',
-  target: '_self',
+  type: "primary",
+  link: "",
+  target: "_self",
+  customStyle: {},
 };
 const Button = (props = defaultProps) => {
   return (
     <a
-      className={
-        classnames({
-          button: true,
-          [`button-${props.type}`]: true,
-        })
-      }
-      target={props.target || '_self'}
-      href={getLink(props.link,props.language)}
+      className={classnames({
+        button: true,
+        [`button-${props.type}`]: true,
+      })}
+      target={props.target || "_self"}
+      href={getLink(props.link, props.language)}
+      style={props.customStyle}
     >
       {props.children}
     </a>
