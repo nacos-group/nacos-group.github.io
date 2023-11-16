d0417e9470846e9ccfe90cd1cd6b380fb5970c78
chai001125 <1637519169@qq.com>
2023-11-16 10:13:58 +0800

    optimize: fix front page errors

--- src/pages/home/feature/index.tsx
+++ src/pages/home/feature/index.tsx
@@ -14,7 +14,19 @@ const data = {
         translate({
           id: 'homepage.featureListContent1',
           message:
-            '动态配置管理、服务发现和动态的一站式解决方案;20多种开箱即用的以服务为中心的架构特性;基本符合生产要求的轻量级易用控制台。',
+            '动态配置管理、服务发现和动态的一站式解决方案',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent2',
+          message:
+            '20多种开箱即用的以服务为中心的架构特性',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent3',
+          message:
+            '基本符合生产要求的轻量级易用控制台',
         }),
       ],
     },
@@ -23,27 +35,67 @@ const data = {
       title: translate({ id: 'homepage.featureListTitle2', message: '更适应云架构' }),
       content: [
         translate({
-          id: 'homepage.featureListContent2',
-          message: '无缝支持Kubernetes和Spring Cloud;在主流公共云上更容易部署和运行（例如阿里云和AWS）;多租户和多环境支持。',
+          id: 'homepage.featureListContent4',
+          message: '无缝支持Kubernetes和Spring Cloud',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent5',
+          message:
+            '在主流公共云上更容易部署和运行（例如阿里云和AWS）',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent6',
+          message:
+            '多租户和多环境支持',
         }),
       ],
     },
     {
       icon: 'img/feature_production_grade.png',
       title: translate({ id: 'homepage.featureListTitle3', message: '生产等级' }),
-      content: translate({
-        id: 'homepage.featureListContent3',
-        message:
-          '脱胎于历经阿里巴巴10年生产验证的内部产品;支持具有数百万服务的大规模场景;具备企业级SLA的开源产品。',
-      }),
+      content: [
+        translate({
+          id: 'homepage.featureListContent7',
+          message:
+            '脱胎于历经阿里巴巴10年生产验证的内部产品',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent8',
+          message:
+            '支持具有数百万服务的大规模场景',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent9',
+          message:
+            '具备企业级SLA的开源产品',
+        }),
+      ],
     },
     {
       icon: 'img/feature_rich.png',
       title: translate({ id: 'homepage.featureListTitle4', message: '丰富的应用场景' }),
-      content: translate({
-        id: 'homepage.featureListContent4',
-        message: '支持限流、大促销预案和异地多活;直接支持或稍作扩展即可支持大量有用的互联网应用场景;流量调度和服务治理。',
-      }),
+      content: [
+        translate({
+          id: 'homepage.featureListContent10',
+          message: '支持限流、大促销预案和异地多活',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent11',
+          message:
+            '直接支持或稍作扩展即可支持大量有用的互联网应用场景',
+        }),
+        <br />,
+        translate({
+          id: 'homepage.featureListContent12',
+          message:
+            '流量调度和服务治理',
+        }),
+      ],
     },
   ],
   title: translate({ id: 'homepage.featureTitle', message: '特色功能' }),
