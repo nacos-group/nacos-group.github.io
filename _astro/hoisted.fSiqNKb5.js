df556895e7c7ce2785c42fd008531a9477540c80
heimanba <heimanba@users.noreply.github.com>
2024-01-17 11:21:14 +0000

    Automated deployment: Wed Jan 17 11:21:14 UTC 2024 6e27165973efae782bc5c8c525c45ae863b0e142

---
+++ _astro/hoisted.fSiqNKb5.js
@@ -0,0 +1,1 @@
+import"./hoisted.keeS8yy8.js";import"./CompaniesFeedback.astro_astro_type_script_index_0_lang.iJchDj8N.js";import"./hoisted.UwFkEG9A.js";import"./index.PDhEm6KS.js";class o extends HTMLElement{constructor(){super()}}customElements.define("cloud-body",o);
---
+++ _astro/hoisted.keeS8yy8.js
@@ -0,0 +1,1 @@
+import"./hoisted.UwFkEG9A.js";import"./index.PDhEm6KS.js";class t extends HTMLElement{constructor(){super()}}customElements.define("footer-layout",t);
