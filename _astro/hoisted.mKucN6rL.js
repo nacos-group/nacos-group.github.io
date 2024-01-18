2d705bfd2ff8ce5ab49378d0ffd04c32187ed2f3
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-01-18 09:19:22 +0000

    Automated deployment: Thu Jan 18 09:19:22 UTC 2024 d3ad4d528d37a1456360d6e78c9d932a978b053e

---
+++ _astro/hoisted.mKucN6rL.js
@@ -0,0 +1,1 @@
+import"./HomeSolutionsCard.astro_astro_type_script_index_0_lang.2hNYnIsh.js";import"./hoisted.FBXcx33Y.js";import"./hoisted.nKiSD5j5.js";import"./index.PDhEm6KS.js";class e extends HTMLElement{constructor(){super()}}customElements.define("home-body",e);
