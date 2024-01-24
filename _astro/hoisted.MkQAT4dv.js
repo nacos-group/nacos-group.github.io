0fe7d2ae9e83486c635822fb384cf96fdfe4a985
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-01-24 09:30:40 +0000

    Automated deployment: Wed Jan 24 09:30:40 UTC 2024 234bd19a9511c922cb8537204c778d3a91f9fd93

---
+++ _astro/hoisted.MkQAT4dv.js
@@ -0,0 +1,1 @@
+import"./CompaniesFeedback.astro_astro_type_script_index_0_lang.iJchDj8N.js";import"./hoisted.YhKranmF.js";import"./hoisted.OD6nPdxh.js";import"./index.PDhEm6KS.js";class o extends HTMLElement{constructor(){super()}}customElements.define("cloud-body",o);
