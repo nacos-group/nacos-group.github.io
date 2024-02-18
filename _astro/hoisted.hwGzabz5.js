ee9bdc02a26b4f8be8b7d25f32c1cd45f26b8aaf
heimanba <heimanba@users.noreply.github.com>
2024-02-18 10:32:21 +0000

    Automated deployment: Sun Feb 18 10:32:21 UTC 2024 0c8507e182a250b6f34d8a097866e4f9909eaa6b

---
+++ _astro/hoisted.hwGzabz5.js
@@ -0,0 +1,1 @@
+import"./hoisted.tE7b3BNr.js";import"./markdown-toc.etSrasMh.js";import"./hoisted.Ezxh7wXc.js";import"./index.PDhEm6KS.js";const o=new URL(window.location.href),r=o.searchParams.get("source"),t=document.querySelector("#back-btn a");t.href=`/${r}/`;
