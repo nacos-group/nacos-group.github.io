53f465acef499370342d65882194d745a86288d4
heimanba <heimanba@users.noreply.github.com>
2024-02-20 04:37:49 +0000

    Automated deployment: Tue Feb 20 04:37:49 UTC 2024 ed87b84d79fa86cffde7f76790835def6a30d723

---
+++ _astro/hoisted.TfbJ4Lb-.js
@@ -0,0 +1,1 @@
+import"./hoisted.ATZuMy9z.js";import"./markdown-toc.etSrasMh.js";import"./hoisted.Ezxh7wXc.js";import"./index.PDhEm6KS.js";const t=new URL(window.location.href);let e=t.searchParams.get("source");e=e.replace(/_/g,"/");const r=document.querySelector("#back-btn a");r.onclick=o=>{o.preventDefault(),location.href=`/${e}/`};
