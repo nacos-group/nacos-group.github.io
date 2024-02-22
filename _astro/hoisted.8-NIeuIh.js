4bfb36abba189859a069fd5093bac05b1f3a6dce
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-02-22 05:38:31 +0000

    Automated deployment: Thu Feb 22 05:38:31 UTC 2024 fc4eff5be87fe9671dee0501279e668e6a3185c8

---
+++ _astro/hoisted.8-NIeuIh.js
@@ -0,0 +1,1 @@
+import"./hoisted.0oYfRBxd.js";import"./markdown-toc.etSrasMh.js";import"./hoisted.kKR2v2Yr.js";import"./index.PDhEm6KS.js";const t=new URL(window.location.href);let e=t.searchParams.get("source");e=e.replace(/_/g,"/");const r=document.querySelector("#back-btn a");r.onclick=o=>{o.preventDefault(),location.href=`/${e}/`};
