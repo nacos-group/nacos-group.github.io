f944e591184f6775f80dd4b1cb2010bfdcd98da8
heimanba <371510756@qq.com>
2024-03-08 10:03:51 +0800

    update By Robot2024-03-08 10:03:51

--- src/consts.ts
+++ src/consts.ts
@@ -105,14 +105,14 @@ export const HEADER_ACTIVITY_CARD = [
     {
         "collection": "blog",
         "slug": "ecosystem-nacos-controller-opensource",
-        "description": "Nacos Controller 项目开源，支持与Kubernetes互通配置",
-        "imageUrl": "https://img.alicdn.com/imgextra/i4/O1CN01bGnAVW1pFeLFnlaQL_!!6000000005331-2-tps-508-370.png"
+        "description": "2222Nacos Controller 项目开源，支持与Kubernetes互通配置",
+        "imageUrl": "==2323==23===5544444444777555---://img.alicdn.com/imgextra/i4/O1CN01bGnAVW1pFeLFnlaQL_!!6000000005331-2-tps-508-370.png"
     },
     {
         "collection": "blog",
         "slug": "announcement-2023-award",
-        "description": "Nacos社区2023年底斩获三项开源大奖",
-        "imageUrl": "https://img.alicdn.com/imgextra/i4/O1CN01zfayJW1Lhe4kliPv9_!!6000000001331-2-tps-508-370.png"
+        "description": "3333社区2023年底斩获三项开源大3奖",
+        "imageUrl": "333333https://img.alicdn.com/imgextra/i4/O1CN01zfayJW1Lhe4kliPv9_!!6000000001331-2-tps-508-370.png"
     }
 ]
 
