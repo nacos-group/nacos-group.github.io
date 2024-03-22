9f6060ba95a51fb15e4b0f007bb45f8fd5241e5b
梓莼 <313658356@qq.com>
2024-03-22 11:30:36 +0800

    fix: markdown docs

--- plugins/remarkRemoveMdLinks.ts
+++ plugins/remarkRemoveMdLinks.ts
@@ -1,38 +1,42 @@
 /**
  * @description: remark插件，删除.md后缀内容
  */
-import { visit } from 'unist-util-visit';
+import { visit } from "unist-util-visit";
 
 export default function remarkRemoveMdLinks() {
-	return (tree) => {
-		visit(tree, 'link', (node) => {
-			// http或者 https 开头的
-			if (node.url.startsWith("http://") || node.url.startsWith("https://") || node.url.startsWith("//")) {
-				return;
-			}
+  return (tree) => {
+    visit(tree, "link", (node) => {
+      // http或者 https 开头的
+      if (
+        node.url.startsWith("http://") ||
+        node.url.startsWith("https://") ||
+        node.url.startsWith("//")
+      ) {
+        return;
+      }
 
-			let isMdxLink = node.url.includes('.mdx')
-      
+      let isMdxLink = node.url.includes(".mdx");
+
       // Process internal markdown links (.md or .mdx)
-      if (node.url.includes('.md') || isMdxLink ) {
+      if (node.url.includes(".md") || isMdxLink) {
         const extensionLength = isMdxLink ? 4 : 3;
-        const parts = node.url.split('#');
-        parts[0] = parts[0].slice(0, -extensionLength) + '/'; // Remove extension and add trailing slash
-        
+        const parts = node.url.split("#");
+        parts[0] = parts[0].slice(0, -extensionLength) + "/"; // Remove extension and add trailing slash
+
         // Ensure relative links start with ./
         if (!/^[.~/]/.test(parts[0])) {
-          parts[0] = './' + parts[0];
+          parts[0] = "./" + parts[0];
         }
-        
-        node.url = parts.join('#');
+
+        node.url = parts.join("#");
       }
 
       // Adjust relative paths
       if (/^\.\//.test(node.url)) {
-        node.url = node.url.replace(/^\.\//, '../');
+        node.url = node.url.replace(/^\.\//, "../");
       } else if (/^\.\.\//.test(node.url)) {
-        node.url = node.url.replace(/^\.\.\//, '../../');
+        node.url = node.url.replace(/^\.\.\//, "../../");
       }
-		});
-	};
+    });
+  };
 }
