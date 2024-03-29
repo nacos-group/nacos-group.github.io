eb8cb4aeae217655198241ac07ae4c2aa4b27b9f
八月 <rentingting.rtt@alibaba-inc.com>
2024-03-29 14:12:58 +0800

    fix: chrome低版本适配

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,89 +1,99 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import locales from './src/i18n/languages';
-import tailwind from '@astrojs/tailwind';
-import rehypeExternalLinks from 'rehype-external-links'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import locales from "./src/i18n/languages";
+import tailwind from "@astrojs/tailwind";
+import rehypeExternalLinks from "rehype-external-links";
 
-import remarkRemoveMdLinks from "./plugins/remarkRemoveMdLinks"
-import remarkRemovePlainLanguageCode from "./plugins/remarkRemovePlainLanguageCode"
-import remarkRemoveRepeatHeader from "./plugins/remarkRemoveRepeatHeader"
-import addPrefixImageLink from "./plugins/addPrefixImageLink"
-import setLinkReferrer from './plugins/remarkReferer'
-import { starlightAsides } from './node_modules/@astrojs/starlight/integrations/asides'
-import goatConfig from './goat.config';
-
+import remarkRemoveMdLinks from "./plugins/remarkRemoveMdLinks";
+import remarkRemovePlainLanguageCode from "./plugins/remarkRemovePlainLanguageCode";
+import remarkRemoveRepeatHeader from "./plugins/remarkRemoveRepeatHeader";
+import addPrefixImageLink from "./plugins/addPrefixImageLink";
+import setLinkReferrer from "./plugins/remarkReferer";
+import { starlightAsides } from "./node_modules/@astrojs/starlight/integrations/asides";
+import goatConfig from "./goat.config";
+import topLevelAwait from "vite-plugin-top-level-await";
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.DEPLOY_SITE || 'https://nacos.io',
-	image: {
-		domain: ["img.alicdn"]
-	},
-	trailingSlash: 'always',
-	integrations: [
-		starlight({
-			title: 'Nacos',
-			favicon: '/favicon.ico',
-			head: [
-			],
-			expressiveCode: {
-				themes: ['github-dark'],
-				// 完全可以自定义
-				// https://github.com/expressive-code/expressive-code/tree/main/packages/astro-expressive-code
-				// https://github.com/shikijs/shiki/blob/main/docs/themes.md#loading-theme
-				// https://vscodethemes.com/ 主题预览
-				// themes: [markdownTheme],
-			},
-			components: {
-				TableOfContents: './src/components/starlight/TableOfContents.astro',
-				Header: './src/components/starlight/Header.astro',
-				Head: './src/components/starlight/Head.astro',
-				Sidebar: './src/components/starlight/Sidebar.astro',
-				PageFrame: './src/components/starlight/PageFrame.astro',
-				SkipLink: './src/components/starlight/SkipLink.astro',
-				TwoColumnContent: './src/components/starlight/TwoColumnContent.astro',
-				PageSidebar: './src/components/starlight/PageSidebar.astro',
-				PageTitle: './src/components/starlight/PageTitle.astro',
-				ContentPanel: './src/components/starlight/ContentPanel.astro',
-				Pagination: './src/components/starlight/Pagination.astro',
-				Banner: './src/components/starlight/Banner.astro'
-			},
-			editLink: {
-				baseUrl: 'https://github.com/nacos-group/nacos-group.github.io/tree/develop-astro-nacos',
-			},
-			locales,
-			customCss: ['./src/style/global.css','./src/style/fonts.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-		{
-			name: '@goat:config',
-			hooks: {
-				"astro:server:setup": async (options) => {
-					await goatConfig();
-				},
-				"astro:build:setup": async (options) => {
-					await goatConfig();
-				}
-			}
-		},
-	],
-	markdown: {
-		rehypePlugins: [
-			// 在这里添加 rehype-external-links 插件配置
-			[rehypeExternalLinks, {
-				target: '_blank'
-			}]],
-		remarkPlugins: [
-			remarkRemoveMdLinks, 
-			remarkRemovePlainLanguageCode, 
-			remarkRemoveRepeatHeader, 
-			addPrefixImageLink,
-			starlightAsides,
-			setLinkReferrer
-		]
-	},
-	redirects: {
-		'/en-us/': '/en/',
-		'/[...slug].html': '/[...slug]',
-		'/docs/': '/docs/latest/what-is-nacos/',
-	}
+  site: process.env.DEPLOY_SITE || "https://nacos.io",
+  image: {
+    domain: ["img.alicdn"],
+  },
+  trailingSlash: "always",
+  integrations: [
+    starlight({
+      title: "Nacos",
+      favicon: "/favicon.ico",
+      head: [],
+      expressiveCode: {
+        themes: ["github-dark"],
+        // 完全可以自定义
+        // https://github.com/expressive-code/expressive-code/tree/main/packages/astro-expressive-code
+        // https://github.com/shikijs/shiki/blob/main/docs/themes.md#loading-theme
+        // https://vscodethemes.com/ 主题预览
+        // themes: [markdownTheme],
+      },
+      components: {
+        TableOfContents: "./src/components/starlight/TableOfContents.astro",
+        Header: "./src/components/starlight/Header.astro",
+        Head: "./src/components/starlight/Head.astro",
+        Sidebar: "./src/components/starlight/Sidebar.astro",
+        PageFrame: "./src/components/starlight/PageFrame.astro",
+        SkipLink: "./src/components/starlight/SkipLink.astro",
+        TwoColumnContent: "./src/components/starlight/TwoColumnContent.astro",
+        PageSidebar: "./src/components/starlight/PageSidebar.astro",
+        PageTitle: "./src/components/starlight/PageTitle.astro",
+        ContentPanel: "./src/components/starlight/ContentPanel.astro",
+        Pagination: "./src/components/starlight/Pagination.astro",
+        Banner: "./src/components/starlight/Banner.astro",
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/nacos-group/nacos-group.github.io/tree/develop-astro-nacos",
+      },
+      locales,
+      customCss: ["./src/style/global.css", "./src/style/fonts.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+    {
+      name: "@goat:config",
+      hooks: {
+        "astro:server:setup": async (options) => {
+          await goatConfig();
+        },
+        "astro:build:setup": async (options) => {
+          await goatConfig();
+        },
+      },
+    },
+  ],
+  markdown: {
+    rehypePlugins: [
+      // 在这里添加 rehype-external-links 插件配置
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+    remarkPlugins: [
+      remarkRemoveMdLinks,
+      remarkRemovePlainLanguageCode,
+      remarkRemoveRepeatHeader,
+      addPrefixImageLink,
+      starlightAsides,
+      setLinkReferrer,
+    ],
+  },
+  redirects: {
+    "/en-us/": "/en/",
+    "/[...slug].html": "/[...slug]",
+    "/docs/": "/docs/latest/what-is-nacos/",
+  },
+  vite: {
+    build: {
+      target: "chrome68",
+    },
+    plugins: [topLevelAwait()],
+  },
 });
--- src/components/common/Header/navbar.ts
+++ src/components/common/Header/navbar.ts
@@ -1,54 +1,53 @@
 import DocsMenu from "./DocsMenu.astro";
 import CommunityMenu from "./CommunityMenu.astro";
 
-
 export default [
   {
-      label: "文档",
-      translations: {
-          en: "DOCS"
-      },
-      trigger: "hover",
-      slot: DocsMenu,
-      position: "absolute",
-      activePath: ['/docs'],
+    label: "文档",
+    translations: {
+      en: "DOCS",
+    },
+    trigger: "hover",
+    slot: DocsMenu,
+    position: "absolute",
+    activePath: ["/docs"],
   },
   {
-      label: "企业版 NACOS",
-      translations: {
-          en: "NACOS CLOUD"
-      },
-      trigger: "click", 
-      route: "/cloud/",
-      target: "_self",
-      activePath: ['/cloud'],
+    label: "企业版 NACOS",
+    translations: {
+      en: "NACOS CLOUD",
+    },
+    trigger: "click",
+    route: "/cloud/",
+    target: "_self",
+    activePath: ["/cloud"],
   },
   {
-      label: "社区",
-      translations: {
-          en: "COMMUNITY"
-      },
-      trigger: "hover",
-      slot: CommunityMenu,
-      position: "fixed",
-      activePath: ['/news', '/activity', '/blog', '/docs/ebook/', '/download'],
+    label: "社区",
+    translations: {
+      en: "COMMUNITY",
     },
-    {
-        label: "博客",
-        translations: {
-            en: "BLOG"
-        },
-        trigger: "click", 
-        target: "_self",
-        route: "/blog/"
-    },
+    trigger: "hover",
+    slot: CommunityMenu,
+    position: "fixed",
+    activePath: ["/news", "/activity", "/blog", "/docs/ebook/", "/download"],
+  },
   {
-      label: "控制台样例",
-      translations: {
-          en: "DEMO"
-      },
-      trigger: "click", 
-      target: "_blank",
-      route: "http://console.nacos.io/nacos/index.html"
-  }
-]
+    label: "博客",
+    translations: {
+      en: "BLOG",
+    },
+    trigger: "click",
+    target: "_self",
+    route: "/blog/",
+  },
+  {
+    label: "控制台样例",
+    translations: {
+      en: "DEMO",
+    },
+    trigger: "click",
+    target: "_blank",
+    route: "http://console.nacos.io/nacos/index.html",
+  },
+];
