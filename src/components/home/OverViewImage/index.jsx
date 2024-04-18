04295a5f669be603144a4b85bff42e8cc4bf639f
梓莼 <313658356@qq.com>
2024-04-18 14:24:30 +0800

    feat: add overview image

--- src/components/home/OverViewImage/index.jsx
+++ src/components/home/OverViewImage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
 import "./style.css";
+import { useState } from "preact/hooks";
 import { Arrow } from "./components/common/Arrow";
 import { Panel } from "./components/common/Panel";
 import { Product } from "./components/common/Product";
@@ -21,15 +22,23 @@ const defaultColorConfig = {
   normalFontColor: "#5C6170",
   highlightFontColor: "#567BE1",
   panelTitleColor: "#1F36AD",
-  tagBgColor:'#e6f4ff',
-  tagBorderColor:'#91caff',
-  tagFontColor:'#1677ff',
-  starForkFontColor:'#8498ee',
-  starForkBgColor:'#ced3f1',
-  linkColor:'#a3e635'
+  tagBgColor: '#e6f4ff',
+  tagBorderColor: '#91caff',
+  tagFontColor: '#1677ff',
+  starForkFontColor: '#8498ee',
+  starForkBgColor: '#ced3f1',
+  linkColor: '#a3e635'
 };
 
-function App({ colors = defaultColorConfig,linkStyle="text-sm",className = '',...props }) {
+function App({ colors = defaultColorConfig, linkStyle = "text-sm", className = '', ...props }) {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const handleImageClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClick = () => {
+    setModalOpen(false);
+  };
   return (
     <AppContext.Provider
       value={{
@@ -48,6 +57,7 @@ function App({ colors = defaultColorConfig,linkStyle="text-sm",className = '',..
         }}
         {...props}
       >
+        <div class='hidden md:inline-block'>
         <div class="flex justify-around flex-1 items-center">
           <GovernancePanel />
           <ControlPanel />
@@ -58,8 +68,41 @@ function App({ colors = defaultColorConfig,linkStyle="text-sm",className = '',..
         <div class="flex justify-around flex-1">
           <OpsPanel />
           <TelemetryPanel />
+          </div> 
+          </div>
+          <img class='md:hidden' alt='overview image' onClick={handleImageClick} src='https://img.alicdn.com/imgextra/i2/O1CN01OhhnzT1CtFbbiKTWq_!!6000000000138-0-tps-2792-1502.jpg' />
+          {isModalOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}
+          onClick={handleModalClick}
+        >
+          <div
+            style={{
+              maxWidth: '80%',
+              maxHeight: '90%',
+              overflow: 'auto',
+            }}
+            onClick={(e) => e.stopPropagation()} // 防止点击图片时也关闭遮罩层
+          >
+            <img
+              src="https://img.alicdn.com/imgextra/i2/O1CN01OhhnzT1CtFbbiKTWq_!!6000000000138-0-tps-2792-1502.jpg"
+              alt="overview image"
+              style={{ maxWidth: '100%' }}
+            />
+          </div>
         </div>
-        {/* <Demo /> */}
+      )}
       </div>
     </AppContext.Provider>
   );
