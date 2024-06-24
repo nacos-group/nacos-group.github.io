7357b6c3950f5b2cb0da8f6911a8ada004625b48
Tinie13 <zhangmj198@163.com>
2024-06-24 15:13:26 +0800

    feat:新增不同版本功能对比

---
+++ src/components/cloud/FunctionalCompare/False.jsx
@@ -0,0 +1,21 @@
+const False = (props) => {
+  return (
+    <svg
+      class="icon"
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      p-id="2296"
+      width="24"
+      height="24">
+      <path
+        d="M806.4 263.2l-45.6-45.6L512 467.2 263.2 217.6l-45.6 45.6L467.2 512 217.6 760.8l45.6 45.6L512 557.6l248.8 248.8 45.6-45.6L557.6 512z"
+        fill="#444444"
+        p-id="2297">
+      </path>
+    </svg>
+
+  );
+};
+
+export default False;
